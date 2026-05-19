d6bd5770b09011f90f3c388266dc370e1773b955
Melwin Abraham <melwinmathenabraham@gmail.com>
2026-05-19 20:19:05 +0100

    revert centering change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,7 @@ export default async function PlanPage({
 
   return (
     <AppShell>
-      <div style={{ maxWidth: "1150px", padding: "0 32px 40px", display: "flex", flexDirection: "column", gap: "12px", margin: "0 auto" }}>
+      <div style={{ maxWidth: "1150px", padding: "0 32px 40px", display: "flex", flexDirection: "column", gap: "12px" }}>
         <RaceHeroCard block={block} />
 
         {pendingDraft && <AdaptBanner draft={pendingDraft} />}
